47ae6df966159b6890e9d961508374d00519a297
DivyankLosse <chinmaykhewale2005@gmail.com>
2026-02-14 21:33:20 +0530

    refactor: Enable Lite Mode (Remove TensorFlow, Mock ML Services)

--- server/main.py
+++ server/main.py
@@ -51,4 +51,4 @@ async def root():
     return {"message": "Agri-Lo API is running 🚀 (Python/FastAPI)"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)

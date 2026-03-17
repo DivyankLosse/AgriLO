5fc2cb8d998fae6268cfe9b00b9b31ba122490fb
Divyank Khewale <chinmaykhewale2005@gmail.com>
2026-03-17 16:21:37 +0530

    Update limiter.py

--- server/utils/limiter.py
+++ server/utils/limiter.py
@@ -1,10 +1,6 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
-def exempt_options(request):
-    return request.method == "OPTIONS"
-
 limiter = Limiter(
-    key_func=get_remote_address,
-    exempt_when=exempt_options
+    key_func=get_remote_address
 )
